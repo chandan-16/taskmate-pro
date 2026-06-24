eeed7bb59906e2de1e60f2999448e8ba3805a6a5
chandan-16 <chandangautam420.cg@gmail.com>
2026-06-24 20:50:07 +0530

    Added the functionality to add edit and delete options to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import { Header } from './components/header/Header'
 import { AddTask } from './components/addTask/AddTask'
 import { ShowTask } from './components/showTask/ShowTask'
@@ -5,13 +7,21 @@ import { ShowTask } from './components/showTask/ShowTask'
 import './App.css'
 
 function App() {
+
+  const [taskList, setTaskList] = useState(JSON.parse(localStorage.getItem("tasklist")) || []);
+  const [task, setTask] = useState({});
   
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(taskList));
+  }, [taskList])
+
+
   return (
     <>
       <div className='App'>
         <Header />
-        <AddTask />
-        <ShowTask />
+        <AddTask taskList={taskList} setTaskList={setTaskList} task={task} setTask={setTask} />
+        <ShowTask taskList={taskList} setTaskList={setTaskList} task={task} setTask={setTask} />
       </div>      
     </>
   )
